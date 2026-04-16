4341f904cc9a01a62621dbd2058e6b77eb6fad18
Amaan Mirza <amaanm3790@gmail.com>
2026-04-15 19:51:31 -0500

    Resolve: linting issues

--- types/express.d.ts
+++ types/express.d.ts
@@ -2,9 +2,8 @@ import { Customer } from '@prisma/client';
 
 declare global {
   namespace Express {
-    // We are intentionally creating an empty interface here to merge declarations.
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    export interface User extends Customer {}
+    // Use a type alias instead of an empty interface
+    export type User = Customer;
 
     export interface Request {
       user?: User;
